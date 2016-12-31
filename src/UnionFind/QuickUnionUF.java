b8cc9e9bdf13eadd4419203f61113b6ca1f53783
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-12-31 14:24:43 +0530

    added root, connected and union method for quick union implementation

--- src/UnionFind/QuickUnionUF.java
+++ src/UnionFind/QuickUnionUF.java
@@ -10,4 +10,22 @@ public class QuickUnionUF {
 			id[i] = i;
 		}
 	}
+	
+	private int root(int i) {
+		while(i != id[i]) {
+			i = id[i];
+		}
+		return i;
+	}
+	
+	public boolean connected(int p, int q) {
+		return root(p) == root(q);
+	}
+	
+	//change the root of p to point to the root of q
+	public void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	} 
 }
