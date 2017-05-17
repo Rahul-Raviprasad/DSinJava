95678e215882c2262212ab0afa5454b4c005bf22
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-05-17 22:42:08 +0530

    adding a binary tree node

---
+++ src/BinarySearchTree/BinaryTreeNode.java
@@ -0,0 +1,29 @@
+package BinarySearchTree;
+
+public class BinaryTreeNode {
+
+	public int data;
+	public BinaryTreeNode left;
+	public BinaryTreeNode right;
+	
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+	
+	
+}
